080adfcbbbc170a6072d0f6c69bfc8ddc45a7545
Cristian <cristianteves1@gmail.com>
2018-12-07 03:39:02 -0300

    finalizando estructura

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 
   for (arrow of arrows){
     arrow.addEventListener('click', function(){
-      this.degrees = this.nextSibling.nextSibling.getAttribute('class') == 'collapse' ? 90 : 0;
+      this.degrees = this.nextSibling.nextSibling.getAttribute('class') == 'submenu collapse' ? 90 : 0;
       this.querySelector('i').style.transform = 'rotate(' + Number(this.degrees) + 'deg)';
       this.querySelector('i').style.transition = 'all ease 0.2s';
     })
